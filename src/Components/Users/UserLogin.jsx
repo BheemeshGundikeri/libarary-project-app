8b0117a4cff03913d5e2ad0583a69fd4b8083926
Bheemesh Gundikeri <bheemesh.gundikeri24@gmail.com>
2025-06-17 00:10:22 +0530

    Use relative base path and fix deploy

--- src/Components/Users/UserLogin.jsx
+++ src/Components/Users/UserLogin.jsx
@@ -1,43 +1,51 @@
-import { useRef } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserLogin = () => {
-  let navigate = useNavigate()
-  let adminform = useRef()
+  const [adminVal, setAdminVal] = useState("user");
+  const [pswdVal, setPswdVal] = useState("12345");
+  const adminform = useRef();
+  const navigate = useNavigate();
 
-  let handleAdminform = (e)=>{
-      e.preventDefault()
+  const handleAdminform = (e) => {
+    e.preventDefault();
 
-      let adminVal = adminform.current[0].value
-      let pswdVal = adminform.current[1].value
+    let adminCredintial = {
+      admin: "user",
+      password: "12345",
+    };
 
-      let adminCredintial = {
-          admin : "user",
-          password : "12345"
-      }
+    let { admin, password } = adminCredintial;
 
-      let {admin, password} = adminCredintial
+    if (adminVal === admin && pswdVal === password) {
+      navigate("./userportal");
+    } else {
+      alert("Pls enter correct details");
+    }
+  };
 
-      if(adminVal === admin && pswdVal === password){
-          navigate("./userportal")
-      }
-      else{
-          alert("Pls enter correct details")
-      }
-  }
   return (
     <>
-       <div className="user_login">
-            <form ref={adminform} onSubmit={handleAdminform}>
-                <input type="username"  placeholder='Enter Email' value="admin" />
-                <input type="password" placeholder='Enter password' value="12345"/>
-
-                <button>User Login</button>
-            </form>
-            <h4> * User Login Page</h4>
-       </div>
+      <div className="user_login">
+        <form ref={adminform} onSubmit={handleAdminform}>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            value={adminVal}
+            onChange={(e) => setAdminVal(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={pswdVal}
+            onChange={(e) => setPswdVal(e.target.value)}
+          />
+          <button type="submit">User Login</button>
+        </form>
+        <h4>* User Login Page</h4>
+      </div>
     </>
   );
-}
+};
 
 export default UserLogin;
